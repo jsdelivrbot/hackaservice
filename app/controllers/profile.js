2ad2fd20112f7d4978197c7fb6dff1656c3fffa8
Admin <admin@Admins-Mac-mini.home>
2018-06-16 11:32:15 -0400

    user fix

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const User = require('../models/user');
 
 exports.getProfile = (req, res, next) => { 
+    console.log('meep: ' + req.params.user);
     User.findOne({ user: req.params.user }
         .exec(function(err, result) {
             if (err) throw err;
