aec82e8c42d149a79b96a77185ccaf90da160073
Admin <admin@Admins-Mac-mini.home>
2018-06-16 10:59:00 -0400

    added user fx

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app      = express();
 var mongoose = require('mongoose');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var cors = require('cors');
  
 var databaseConfig = require('./app/config/database');
 var router = require('./app/routes');
@@ -13,7 +12,13 @@ mongoose.connect(databaseConfig.url);
 app.listen(process.env.PORT || 8090);
 console.log("App listening on port 8090");
  
-app.use(cors());
+app.all('/*', (req, res, next) => { 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    next();
+})
+
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
