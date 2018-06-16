f4e7edaa7c42897c04cdbb978313d5723cf00631
Admin <admin@Admins-Mac-mini.home>
2018-06-16 11:43:48 -0400

    fixed now

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -3,10 +3,9 @@ const User = require('../models/user');
 
 exports.getProfile = (req, res, next) => { 
     console.log('meep: ' + req.params.user);
-    User.findOne({ email: req.params.user }
+    User.findOne({ email: req.params.user })
         .exec(function(err, result) {
             if (err) throw err;
             res.json(result);
-        })
-    );
+        });
 }
