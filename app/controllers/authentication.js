1a5a5caef5fb53f53f9e09e84a6b5e7f72c1b694
Joe Tortorelli <jtortorelli@ups.com>
2018-07-12 15:31:28 -0400

    I think Im onto something

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -19,10 +19,11 @@ function setUserInfo(request){
 exports.login = function(req, res, next){
     console.log('test');
     var userInfo = setUserInfo({ _id: req._id, email: req.email, role:req.role });
+    let userInfo2 = setUserInfo(req.user);
     console.log('moar test');
     res.status(200).json({
         token: 'JWT ' + generateToken(userInfo),
-        user: userInfo
+        user: userInfo2
     });
 }
 exports.register = function(req, res, next){
